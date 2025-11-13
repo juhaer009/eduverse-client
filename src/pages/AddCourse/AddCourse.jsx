b4f228d10e1b3ebf83376824a6d7786f72c9f34e
juhaer009 <juhaerjafid1998@gmail.com>
2025-11-13 22:40:04 +0600

    created add-course form

--- src/pages/AddCourse/AddCourse.jsx
+++ src/pages/AddCourse/AddCourse.jsx
@@ -1,19 +1,103 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const AddCourse = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div>
-            <a className="link link-hover">Forgot password?</a>
-          </div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+    <div>
+      <h2 className="font-semibold text-secondary text-center text-4xl mt-6">Add a Course</h2>
+      <div className=" flex justify-center items-center my-15">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <fieldset className="fieldset">
+              {/* name */}
+              <label className="label">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="Name"
+                defaultValue={user.displayName}
+                readOnly
+              />
+              {/* email */}
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+                defaultValue={user.email}
+                readOnly
+              />
+              {/* photoURL */}
+              <label className="label">Photo URL</label>
+              <input
+                type="text"
+                name="photo"
+                className="input"
+                placeholder="Photo URL"
+                defaultValue={user.photoURL}
+                readOnly
+              />
+              {/* course title */}
+              <label className="label">Course Title</label>
+              <input
+                type="text"
+                name="title"
+                className="input"
+                placeholder="Course Title"
+                required
+              />
+              {/* course image */}
+              <label className="label">Course Image URL</label>
+              <input
+                type="text"
+                name="courseImage"
+                className="input"
+                placeholder="Course Image URL"
+                required
+              />
+              {/* price */}
+              <label className="label">Course Price</label>
+              <input
+                type="text"
+                name="price"
+                className="input"
+                placeholder="Course Price"
+                required
+              />
+              {/* duration */}
+              <label className="label">Course Duration</label>
+              <input
+                type="text"
+                name="duration"
+                className="input"
+                placeholder="Course Duration"
+                required
+              />
+              {/* category */}
+              <label className="label">Course Category</label>
+              <input
+                type="text"
+                name="category"
+                className="input"
+                placeholder="Course Category"
+                required
+              />
+              {/* description */}
+              <label className="label">Course Description</label>
+              <input
+                type=""
+                name="description"
+                className="input"
+                placeholder="Course Description"
+                required
+              />
+              <button className="btn btn-primary mt-4">Add Course</button>
+            </fieldset>
+          </form>
+        </div>
       </div>
     </div>
   );
