7919692876257d16708842286598aaec79b18bbf
juhaer009 <juhaerjafid1998@gmail.com>
2025-11-13 22:07:34 +0600

    added spinner for routing

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router';
-import Footer from '../components/Footer';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { Outlet, useLocation } from "react-router";
+import Footer from "../components/Footer";
+import Loading from "../components/Loading/Loading";
 
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const location = useLocation();
+  const [routeLoading, setRouteLoading] = useState();
+  useEffect(() => {
+    setRouteLoading(true);
+    const timer = setTimeout(() => {
+      setRouteLoading(false);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [location]);
+  if (routeLoading) {
+    return <Loading></Loading>;
+  }
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
